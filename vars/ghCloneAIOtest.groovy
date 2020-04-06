58b95d6b9fde5371252576f86fd3029d0eb985ef
James Cahill <jcahill773@gmail.com>
2020-04-06 14:42:00 +0100

    Update ghCloneAIOtest.groovy

--- vars/ghCloneAIOtest.groovy
+++ vars/ghCloneAIOtest.groovy
@@ -1,10 +1,17 @@
 // Scripted Pipelines only.
 
 def call() {
+  def String sha
   docker.image('alpine:latest').inside {
         stage('Clone') {
             sh 'apk add git'
-			ghClone()
+			
+			git "https://github.com/${REPO}.git"
+			
+			sha = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'")
+			echo "sha = ${sha}"
+			
+			stash name: 'scm', includes: '*'
         }
     }
 }
