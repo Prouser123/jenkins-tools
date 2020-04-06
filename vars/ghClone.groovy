5781b23c260006f18d47a0dc282c08f2e424eaf4
James Cahill <jcahill773@gmail.com>
2020-04-06 14:09:49 +0100

    Update ghClone.groovy

--- vars/ghClone.groovy
+++ vars/ghClone.groovy
@@ -1,7 +1,7 @@
 // vars/ghClone.groovy
 
 def call() {
-  git 'https://github.com/$REPO.git'
+  git "https://github.com/$REPO.git"
   echo 'GIT COMMIT: $GIT_COMMIT'
   stash name: 'scm', includes: '*'
 }
