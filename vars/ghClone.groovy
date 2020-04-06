bba641f46bff209c65f9583b15be2c76a4782874
James Cahill <jcahill773@gmail.com>
2020-04-06 13:58:12 +0100

    Update ghClone.groovy

--- vars/ghClone.groovy
+++ vars/ghClone.groovy
@@ -1,12 +1,7 @@
 // vars/ghClone.groovy
 
 def call() {
-    stage('Clone') {
-         agent { label 'docker-cli', docker {image 'alpine:latest'}
-         steps {
             git 'https://github.com/${env.REPO}.git'
 			echo 'GIT COMMIT: $GIT_COMMIT'
 			stash name 'scm', includes '*'
-         }
-      }
 }
