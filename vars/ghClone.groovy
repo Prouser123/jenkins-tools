e272f7e060e27f2614cd66f7f92f130ac3650e32
James Cahill <jcahill773@gmail.com>
2020-04-06 14:36:49 +0100

    hotfix

--- vars/ghClone.groovy
+++ vars/ghClone.groovy
@@ -1,7 +1,7 @@
 // vars/ghClone.groovy
 
 def call() {
-  git "https://github.com/${env.REPO}.git"
+  git "https://github.com/${REPO}.git"
   echo "gitCommit() | ${gitCommit()}"
   stash name: 'scm', includes: '*'
 }
