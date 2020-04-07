078afa6dd2837fab3177e1702fbbae2d1bc41bc4
James Cahill <jcahill773@gmail.com>
2020-04-07 14:10:01 +0100

    Fix ghSetStatus and gitCommit scripts

--- vars/ghSetStatus.groovy
+++ vars/ghSetStatus.groovy
@@ -3,7 +3,7 @@
 def call(String message, String state, String context="ci") {
 	withCredentials([string(credentialsId: 'github-ci', variable: 'TOKEN')]) {
 		sh """
-            curl --silent --output /dev/null --show-error --fail \"https://api.github.com/repos/	${REPO}/statuses/${gitCommit()}\" \
+            curl --silent --output /dev/null --show-error --fail \"https://api.github.com/repos/${REPO}/statuses/${gitCommit()}\" \
 				-H \"Authorization: ${TOKEN}\" \
                 -H \"Content-Type: application/json\" \
                 -X POST \
--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,5 +1,5 @@
 // vars/gitCommit.groovy
 
 def call() {
-  return sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'")
+  return sh(returnStdout: true, script: "git rev-parse HEAD")
 }
