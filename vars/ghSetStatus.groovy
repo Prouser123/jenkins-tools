5ab7ff7c70acea8eb2b9d94ee23f230b29ab1315
James Cahill <jcahill773@gmail.com>
2020-04-07 17:49:28 +0100

    Update ghSetStatus to not rely on REPO env variable

--- vars/ghSetStatus.groovy
+++ vars/ghSetStatus.groovy
@@ -1,9 +1,13 @@
 // vars/ghSetStatus.groovy
 
 def call(String message, String state, String context="ci") {
+	
+	String origin = sh(returnStdout: true, script: "git config --get remote.origin.url")
+	String repo = ((origin.split("/")[-2]).split(".")[0])
+	
 	withCredentials([string(credentialsId: 'github-ci', variable: 'TOKEN')]) {
 		sh """
-            curl --silent --output /dev/null --show-error --fail \"https://api.github.com/repos/${REPO}/statuses/${gitCommit()}\" \
+            curl --silent --output /dev/null --show-error --fail \"https://api.github.com/repos/$repo/statuses/${gitCommit()}\" \
 				-H \"Authorization: token ${TOKEN}\" \
                 -H \"Content-Type: application/json\" \
                 -X POST \
