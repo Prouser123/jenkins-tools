aac849c4d4e6402ac2a446d0684ccda08b5f346d
James Cahill <jcahill773@gmail.com>
2020-04-08 20:22:18 +0100

    gitBranch, fallback to GIT_BRANCH when cli not available, add debug logs

--- vars/gitBranch.groovy
+++ vars/gitBranch.groovy
@@ -1,5 +1,16 @@
 // vars/gitBranch.groovy
 
 def call() {
-  return sh(returnStdout: true, script: "git rev-parse --abbrev-ref HEAD").replaceAll('\r', '').replaceAll('\n', '')
+  String gitCli = sh(returnStdout: true, script: "git rev-parse --abbrev-ref HEAD").replaceAll('\r', '').replaceAll('\n', '')
+  
+  if (gitCli != "HEAD") {
+    // command returned actual branch, return output.
+    echo '[gitBranch] Found using CLI.'
+	return gitCli
+
+  } else {
+    // command returned HEAD, so we must be using a multi-branch pipeline, falling back to $GIT_BRANCH instead.
+    echo '[gitBranch] Found using ENV.'
+	return GIT_BRANCH
+  }
 }
