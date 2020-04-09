8c48b016b3772ce562a61537afeab84c56c28df1
James Cahill <jcahill773@gmail.com>
2020-04-09 13:32:52 +0100

    [gitCommit] use env.GIT_COMMIT when available

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,5 +1,13 @@
 // vars/gitCommit.groovy
 
 def call() {
-  return sh(returnStdout: true, script: "git rev-parse HEAD").replaceAll('\r', '').replaceAll('\n', '')
+  if (env.GIT_COMMIT) {
+  
+    echo "[gitCommit] Using env.GIT_COMMIT. (value: ${env.GIT_COMMIT})"
+	return env.GIT_COMMIT
+  } else {
+  
+    echo "[gitCommit] env.GIT_COMMIT not available, using git CLI instead..."
+    return sh(returnStdout: true, script: "git rev-parse HEAD").replaceAll('\r', '').replaceAll('\n', '')
+  }
 }
--- vars/scmCloneStage.groovy
+++ vars/scmCloneStage.groovy
@@ -10,14 +10,17 @@ def call() {
 			
 			echo "[scmCloneStage] found GIT_BRANCH: ${scmVars.GIT_BRANCH}"
 			echo "[scmCloneStage] found GIT_URL: ${scmVars.GIT_URL}"
+			echo "[scmCloneStage] found GIT_COMMIT: ${scmVars.GIT_COMMIT}"
 			
 			env.GIT_BRANCH = scmVars.GIT_BRANCH
 			env.GIT_URL = scmVars.GIT_URL
+			env.GIT_COMMIT = scmVars.GIT_COMMIT
 			
 			echo "[scmCloneStage] set env.GIT_BRANCH: ${env.GIT_BRANCH}"
 			echo "[scmCloneStage] set env.GIT_URL: ${env.GIT_URL}"
+			echo "[scmCloneStage] set env.GIT_COMMIT: ${env.GIT_COMMIT}
 			
-			echo "SHA = ${gitCommit()}"
+			// echo "SHA = ${gitCommit()}"
 			
 			stash name: 'scm', includes: '**'
         }
