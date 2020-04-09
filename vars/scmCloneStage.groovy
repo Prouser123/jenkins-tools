b03a7f0af9ba0241762220fd30745c0622914d18
James Cahill <jcahill773@gmail.com>
2020-04-09 13:13:39 +0100

    [ghSetStatus] use GIT_URL when available as .git is somehow not always present

--- vars/scmCloneStage.groovy
+++ vars/scmCloneStage.groovy
@@ -9,10 +9,13 @@ def call() {
 			def scmVars = checkout scm
 			
 			echo "[scmCloneStage] found GIT_BRANCH: ${scmVars.GIT_BRANCH}"
+			echo "[scmCloneStage] found GIT_URL: ${scmVars.GIT_URL}"
 			
 			env.GIT_BRANCH = scmVars.GIT_BRANCH
+			env.GIT_URL = scmVars.GIT_URL
 			
 			echo "[scmCloneStage] set env.GIT_BRANCH: ${env.GIT_BRANCH}"
+			echo "[scmCloneStage] set env.GIT_URL: ${env.GIT_URL}"
 			
 			echo "SHA = ${gitCommit()}"
 			
