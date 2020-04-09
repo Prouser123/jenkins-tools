4494cafe3e596b539889292e6f842271b7b12674
James Cahill <jcahill773@gmail.com>
2020-04-09 13:18:57 +0100

    Hotfix for property not available

--- vars/ghSetStatus.groovy
+++ vars/ghSetStatus.groovy
@@ -2,17 +2,19 @@
 
 def call(String message, String state, String context="ci") {
 
+	String origin
+
 	if (env.GIT_URL) {
 		echo "[ghSetStatus] GIT_URL available, value: ${env.GIT_URL}"
 		
 		// We are in a multibranch pipeline where .git may not be available (https://ci.jcx.ovh/job/discord-xbl/job/master/20/console) so we will use the ENV variable instead.
-		String origin = env.GIT_URL
+		origin = env.GIT_URL
 		
 	} else {
 		echo "[ghSetStatus] GIT_URL not available, using git CLI instead..."
 		
 		// The env variable is not available, falling back to CLI.
-		String origin = sh(returnStdout: true, script: "git config --get remote.origin.url").replaceAll('\r', '').replaceAll('\n', '')
+		origin = sh(returnStdout: true, script: "git config --get remote.origin.url").replaceAll('\r', '').replaceAll('\n', '')
 	}
 	String[] originArr = origin.split("/")
 	String repo = (originArr[-2] + "/" + originArr[-1]).replace(".git", "")
