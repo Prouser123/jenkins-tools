3eaf08101436654aeaf1fb4d783684e1f852406c
James Cahill <jcahill773@gmail.com>
2020-04-13 14:37:00 +0100

    split into seperate files to make compiler happy

--- vars/postJob.groovy
+++ vars/postJob.groovy
@@ -11,12 +11,7 @@ import java.lang.Exception
 
 // Not Built // Unstable is not supported.
 
-// Default function to call when the caller does not specify the called on[..] Closure.
-def default() {
-  echo '[postJob] No script specified.'
-}
-
-def call(Closure body, Closure onSuccess=default, Closure onFailure=default, Closure onAborted=default) {
+def call(Closure body, Closure onSuccess=postJobDefaultFunction, Closure onFailure=postJobDefaultFunction, Closure onAborted=postJobDefaultFunction) {
   try {
     body()
   }
---
+++ vars/postJobDefaultFunction.groovy
@@ -0,0 +1,6 @@
+// vars/postJobDefaultFunction.groovy
+
+// Default function to call when the caller does not specify the called on[..] Closure.
+def call() {
+  echo '[postJob] No script specified.'
+}
